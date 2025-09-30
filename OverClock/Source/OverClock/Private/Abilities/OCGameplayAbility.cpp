379124ca95ba9741e7ab54000646d2b1e3a5cbd9
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-09-30 13:44:50 +0900

    `UOCGameplayAbility 클래스 추가 및 능력 활성화 정책 로직 구현`

---
+++ OverClock/Source/OverClock/Private/Abilities/OCGameplayAbility.cpp
@@ -0,0 +1,42 @@
+#include "Abilities/OCGameplayAbility.h"
+
+#include "AbilitySystemComponent.h"
+#include "Abilities/OCAbilitySystemComponent.h"
+
+UOCGameplayAbility::UOCGameplayAbility()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UOCGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+	if (ActivationPolicy == EOCAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo && !Spec.IsActive())
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
+
+void UOCGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	if (ActivationPolicy == EOCAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
+}
+
+UOCAbilitySystemComponent* UOCGameplayAbility::GetOCASC() const
+{
+	return Cast<UOCAbilitySystemComponent>(CurrentActorInfo ? CurrentActorInfo->AbilitySystemComponent : nullptr);
+}
+
+
---
+++ OverClock/Source/OverClock/Public/Abilities/OCGameplayAbility.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "OCGameplayAbility.generated.h"
+
+
+class UOCAbilitySystemComponent;
+
+UENUM(BlueprintType)
+enum class EOCAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
+UCLASS()
+class OVERCLOCK_API UOCGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UOCGameplayAbility();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "OC|Ability")
+	EOCAbilityActivationPolicy ActivationPolicy = EOCAbilityActivationPolicy::OnTriggered;
+
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	UFUNCTION(BlueprintPure, Category = "OC|Ability")
+	UOCAbilitySystemComponent* GetOCASC() const;
+};
