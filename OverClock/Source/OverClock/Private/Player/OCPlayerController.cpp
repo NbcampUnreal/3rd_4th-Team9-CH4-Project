0f277777087bf34d66a5c8ed50be3db1b78338f9
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-09 19:59:41 +0900

    `입력 매핑 처리 방식 리팩토링 및 불필요 코드 제거`

--- OverClock/Source/OverClock/Private/Player/OCPlayerController.cpp
+++ OverClock/Source/OverClock/Private/Player/OCPlayerController.cpp
@@ -1,165 +1,46 @@
 #include "Player/OCPlayerController.h"
-#include "Input/OCInputComponent.h"
-#include "Data/DA_OCInputConfig.h"
-#include "Data/OCGameplayTags.h"
 #include "EnhancedInputSubsystems.h"
-#include "InputActionValue.h"
-#include "GameplayTagContainer.h"
-#include "GameFramework/Pawn.h"
-#include "Player/OCCharacterBase.h"
-#include "Player/OCPlayerState.h"
-#include "AbilitySystemComponent.h"
-
-
-AOCPlayerController::AOCPlayerController()
-{
-	InputComponent = CreateDefaultSubobject<UOCInputComponent>(TEXT("InputComponent"));
-}
-
-
 
 void AOCPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+	AddDefaultMapping();
+}
 
-	checkf(InputConfigDataAsset,TEXT("Forgot to assign a valid data asset as input config"));
-	
-	if (IsLocalController())
+void AOCPlayerController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	AddDefaultMapping();
+}
+
+void AOCPlayerController::OnUnPossess()
+{
+	RemoveDefaultMapping();
+	Super::OnUnPossess();
+}
+
+void AOCPlayerController::AddDefaultMapping() const
+{
+	if (!IsLocalController() || !DefaultMappingContext) return;
+
+	if (const ULocalPlayer* LocalPlayer = GetLocalPlayer())
 	{
-		if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
 		{
-			if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
-			{
-				Subsystem->AddMappingContext(InputConfigDataAsset->DefaultMappingContext, 0);
-			}
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
 		}
 	}
 }
 
-void AOCPlayerController::SetupInputComponent()
+void AOCPlayerController::RemoveDefaultMapping() const
 {
-	Super::SetupInputComponent();
+	if (!IsLocalController() || !DefaultMappingContext) return;
 
-	UOCInputComponent* OCInputComponent = Cast<UOCInputComponent>(InputComponent);
-	if (!ensureAlwaysMsgf(OCInputComponent, TEXT("InputComponent must be UOCInputComponent"))) return;
-	if (!ensureAlways(InputConfigDataAsset)) return;
-
-	OCInputComponent->BindNativeInputAction(InputConfigDataAsset, OCGameplayTags::InputTag_Move, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
-	OCInputComponent->BindNativeInputAction(InputConfigDataAsset, OCGameplayTags::InputTag_Look, ETriggerEvent::Triggered, this, &ThisClass::Input_Look);
-	OCInputComponent->BindNativeInputAction(InputConfigDataAsset, OCGameplayTags::InputTag_Jump, ETriggerEvent::Triggered, this, &ThisClass::Input_Jump_Pressed);
-	OCInputComponent->BindNativeInputAction(InputConfigDataAsset, OCGameplayTags::InputTag_Jump, ETriggerEvent::Completed, this, &ThisClass::Input_Jump_Released);
-
-	OCInputComponent->BindAbilityInputAction(InputConfigDataAsset, this, &ThisClass::Input_Ability_Pressed, &ThisClass::Input_Ability_Pressed);
-}
-
-
-void AOCPlayerController::Input_Move(const FInputActionValue& Value)
-{
-	APawn* P = GetPawn();
-	if (!P) return;
-	
-	const FVector2D MovementVector = Value.Get<FVector2D>();
-	if (MovementVector.IsNearlyZero()) return;
-	
-	const FRotator MovementRotation = FRotator(0.0f, GetControlRotation().Yaw, 0.0f);
-
-	if (!FMath::IsNearlyZero(MovementVector.X))
+	if (const ULocalPlayer* LocalPlayer = GetLocalPlayer())
 	{
-		const FVector ForwardDirection = MovementRotation.RotateVector(FVector::ForwardVector);
-		P->AddMovementInput(ForwardDirection, MovementVector.X);
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			Subsystem->RemoveMappingContext(DefaultMappingContext);
+		}
 	}
-
-	if (!FMath::IsNearlyZero(MovementVector.Y))
-	{
-		const FVector RightDirection = MovementRotation.RotateVector(FVector::RightVector);
-		P->AddMovementInput(RightDirection, MovementVector.Y);
-	}
-}
-
-void AOCPlayerController::Input_Look(const FInputActionValue& Value)
-{
-	const FVector2D LookVector = Value.Get<FVector2D>();
-
-	if (!FMath::IsNearlyZero(LookVector.X))
-	{
-		AddYawInput(LookVector.X);
-	}
-
-	if (!FMath::IsNearlyZero(LookVector.Y))
-	{
-		AddPitchInput(LookVector.Y);
-	}
-}
-
-void AOCPlayerController::Input_Jump_Pressed(const FInputActionValue& Value)
-{
-	ACharacter* C = GetCharacter();
-	if (C)
-	{
-		C->Jump();
-	}
-
-	/*if (AOCCharacterBase* C = Cast<AOCCharacterBase>(GetPawn()))
-	{
-		C->Jump();
-	}*/
-}
-
-void AOCPlayerController::Input_Jump_Released(const FInputActionValue& Value)
-{
-	ACharacter* C = GetCharacter();
-	if (C)
-	{
-		C->StopJumping();
-	}
-
-	/*if (AOCCharacterBase* C = Cast<AOCCharacterBase>(GetPawn()))
-	{
-		C->StopJumping();
-	}*/
-}
-
-void AOCPlayerController::Input_Ability_Pressed(FGameplayTag InInputTag)
-{
-	if (!InInputTag.IsValid()) return;
-
-	if (UAbilitySystemComponent* ASC = GetOCASC())
-	{
-		FGameplayTagContainer TagContainer;
-		TagContainer.AddTag(InInputTag);
-		ASC->TryActivateAbilitiesByTag(TagContainer);
-	}
-	Server_TryActivateByTag(InInputTag);
-}
-
-void AOCPlayerController::Input_Ability_Released(FGameplayTag InInputTag)
-{
-	if (!InInputTag.IsValid()) return;
-	Server_InputReleased_ByTag(InInputTag);
-}
-
-void AOCPlayerController::Server_InputReleased_ByTag_Implementation(FGameplayTag InInputTag)
-{
-	
-}
-
-void AOCPlayerController::Server_TryActivateByTag_Implementation(FGameplayTag InInputTag)
-{
-	if (!InInputTag.IsValid()) return;
-
-	if (UAbilitySystemComponent* ASC = GetOCASC())
-	{
-		FGameplayTagContainer TagContainer;
-		TagContainer.AddTag(InInputTag);
-		ASC->TryActivateAbilitiesByTag(TagContainer);
-	}
-}
-
-UAbilitySystemComponent* AOCPlayerController::GetOCASC() const
-{
-	if (const AOCPlayerState* PS = GetPlayerState<AOCPlayerState>())
-	{
-		return PS->GetAbilitySystemComponent();
-	}
-	return nullptr;
 }
--- OverClock/Source/OverClock/Public/Player/OCPlayerController.h
+++ OverClock/Source/OverClock/Public/Player/OCPlayerController.h
@@ -4,45 +4,24 @@
 #include "GameFramework/PlayerController.h"
 #include "OCPlayerController.generated.h"
 
-class UDA_OCInputConfig;
-class UOCInputComponent;
-class UAbilitySystemComponent;
-class UEnhancedInputLocalPlayerSubsystem;
-struct FInputActionValue;
-struct FGameplayTag;
+class UInputMappingContext;
 
 UCLASS()
 class OVERCLOCK_API AOCPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 public:
-	AOCPlayerController();
-	
 	virtual void BeginPlay() override;
-	virtual void SetupInputComponent() override;
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext = nullptr;
 
 private:
+	void AddDefaultMapping() const;
+	void RemoveDefaultMapping() const;
 
-#pragma region Inputs
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PlayerData", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UDA_OCInputConfig> InputConfigDataAsset;
-	
-	void Input_Move(const FInputActionValue& Value);
-	void Input_Look(const FInputActionValue& Value);
-	void Input_Jump_Pressed(const FInputActionValue& Value);
-	void Input_Jump_Released(const FInputActionValue& Value);
-
-	void Input_Ability_Pressed(FGameplayTag InInputTag);
-	void Input_Ability_Released(FGameplayTag InInputTag);
-	
-	UFUNCTION(Server, Reliable)
-	void Server_TryActivateByTag(FGameplayTag InInputTag);
-
-	UFUNCTION(Client, Reliable)
-	void Server_InputReleased_ByTag(FGameplayTag InInputTag);
-
-	
-	UAbilitySystemComponent* GetOCASC() const;
-#pragma endregion
 	
 };
