972db6ac1beb8679d19cb3184942e13775fb5f82
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-10 20:44:37 +0900

    `UGA_CrunchBase 신규 클래스 추가`

---
+++ OverClock/Source/OverClock/Public/Abilities/GA_CrunchBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/OCGameplayAbility.h"
+#include "GA_CrunchBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERCLOCK_API UGA_CrunchBase : public UOCGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
