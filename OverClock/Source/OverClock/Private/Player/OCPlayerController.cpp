4a963ef2e245b04fae84d41e3d30e46256ce8a8f
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-10 09:46:11 +0900

    `입력 매핑 처리 로직 리팩토링 및 FallbackMappingContext 추가`

--- OverClock/Source/OverClock/Private/Player/OCPlayerController.cpp
+++ OverClock/Source/OverClock/Private/Player/OCPlayerController.cpp
@@ -1,46 +1,85 @@
 #include "Player/OCPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "Player/OCCharacterBase_V2.h"
+#include "Data/DA_OCInputConfig.h"
+
+AOCPlayerController::AOCPlayerController()
+{
+    bReplicates = true;
+}
 
 void AOCPlayerController::BeginPlay()
 {
-	Super::BeginPlay();
-	AddDefaultMapping();
+    Super::BeginPlay();
 }
 
 void AOCPlayerController::OnPossess(APawn* InPawn)
 {
-	Super::OnPossess(InPawn);
-	AddDefaultMapping();
+    Super::OnPossess(InPawn);
+    
+    UInputMappingContext* IMC = ResolvePawnMappingContext(InPawn);
+    
+    if (!IMC && FallbackMappingContext)
+    {
+        IMC = FallbackMappingContext;
+    }
+    
+    ApplyMappingContext(IMC,0);
 }
 
 void AOCPlayerController::OnUnPossess()
 {
-	RemoveDefaultMapping();
-	Super::OnUnPossess();
+    RemoveMappingContext();
+
+    Super::OnUnPossess();
 }
 
-void AOCPlayerController::AddDefaultMapping() const
-{
-	if (!IsLocalController() || !DefaultMappingContext) return;
 
-	if (const ULocalPlayer* LocalPlayer = GetLocalPlayer())
-	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
-		{
-			Subsystem->AddMappingContext(DefaultMappingContext, 0);
-		}
-	}
+UInputMappingContext* AOCPlayerController::ResolvePawnMappingContext(APawn* InPawn) const
+{
+    if (!InPawn) return nullptr;
+    
+    if (const AOCCharacterBase_V2* V2 = Cast<AOCCharacterBase_V2>(InPawn))
+    {
+        if (const UDA_OCInputConfig* IC = V2->GetInputConfig())
+        {
+            return IC->DefaultMappingContext;
+        }
+    }
+    
+    return nullptr;
 }
 
-void AOCPlayerController::RemoveDefaultMapping() const
+void AOCPlayerController::ApplyMappingContext(UInputMappingContext* IMC, int32 Priority)
 {
-	if (!IsLocalController() || !DefaultMappingContext) return;
+    if (!IMC) return;
+    
+    if (AppliedMappingContext == IMC)
+    {
+        return;
+    }
+    
+    RemoveMappingContext();
 
-	if (const ULocalPlayer* LocalPlayer = GetLocalPlayer())
-	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
-		{
-			Subsystem->RemoveMappingContext(DefaultMappingContext);
-		}
-	}
+    ULocalPlayer* LocalPlayer = GetLocalPlayer();
+    UEnhancedInputLocalPlayerSubsystem* Subsys = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+
+    check(Subsys);
+    
+    Subsys->AddMappingContext(IMC, Priority);
+    AppliedMappingContext = IMC;
+}
+
+void AOCPlayerController::RemoveMappingContext()
+{
+    if (!AppliedMappingContext) return;
+
+    ULocalPlayer* LocalPlayer = GetLocalPlayer();
+    UEnhancedInputLocalPlayerSubsystem* Subsys = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+
+    check(Subsys);
+    
+    Subsys->RemoveMappingContext(AppliedMappingContext);
+    AppliedMappingContext = nullptr;
 }
--- OverClock/Source/OverClock/Public/Player/OCPlayerController.h
+++ OverClock/Source/OverClock/Public/Player/OCPlayerController.h
@@ -5,23 +5,32 @@
 #include "OCPlayerController.generated.h"
 
 class UInputMappingContext;
+class UEnhancedInputLocalPlayerSubsystem;
+class UDA_OCInputConfig;
 
 UCLASS()
 class OVERCLOCK_API AOCPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
 public:
+	AOCPlayerController();
+
 	virtual void BeginPlay() override;
 	virtual void OnPossess(APawn* InPawn) override;
 	virtual void OnUnPossess() override;
-
+	
 protected:
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	UInputMappingContext* DefaultMappingContext = nullptr;
+	UPROPERTY(EditDefaultsOnly, Category="OC|Input")
+	TObjectPtr<UInputMappingContext> FallbackMappingContext = nullptr;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UInputMappingContext> AppliedMappingContext = nullptr;
+	
 
 private:
-	void AddDefaultMapping() const;
-	void RemoveDefaultMapping() const;
-
+	UInputMappingContext* ResolvePawnMappingContext(APawn* InPawn) const;
 	
+	void ApplyMappingContext(UInputMappingContext* IMC, int32 Priority = 0);
+	void RemoveMappingContext();
 };
