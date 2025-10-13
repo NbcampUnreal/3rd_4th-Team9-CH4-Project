0f8926246487acd7130563f9e8fd342972dec35a
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-13 22:14:17 +0900

    `UOCAttributeSet_Resources 신규 추가 및 UltCharge 속성 복제/클램프 로직 구현`

---
+++ OverClock/Source/OverClock/Private/Abilities/Attributes/OCAttributeSet_Resources.cpp
@@ -0,0 +1,36 @@
+#include "Abilities/Attributes/OCAttributeSet_Resources.h"
+#include "Net/UnrealNetwork.h"
+#include "GameplayEffectExtension.h"
+
+void UOCAttributeSet_Resources::OnRep_UltCharge(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UOCAttributeSet_Resources, UltCharge, OldValue);
+}
+
+void UOCAttributeSet_Resources::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UOCAttributeSet_Resources, UltCharge, COND_None, REPNOTIFY_Always);
+}
+
+void UOCAttributeSet_Resources::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetUltChargeAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, 100.f);
+	}
+}
+
+void UOCAttributeSet_Resources::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	if (Data.EvaluatedData.Attribute == GetUltChargeAttribute())
+	{
+		const float Clamped = FMath::Clamp(UltCharge.GetCurrentValue(), 0.f, 100.f);
+		UltCharge.SetCurrentValue(Clamped);
+		UltCharge.SetBaseValue(Clamped);
+	}
+}
---
+++ OverClock/Source/OverClock/Public/Abilities/Attributes/OCAttributeSet_Resources.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Attributes/OCAttributeSetBase.h"
+#include "OCAttributeSet_Resources.generated.h"
+
+UCLASS()
+class OVERCLOCK_API UOCAttributeSet_Resources : public UOCAttributeSetBase
+{
+	GENERATED_BODY()
+
+public:
+	UOCAttributeSet_Resources(){}
+
+	UPROPERTY(BlueprintReadOnly, Category = "Resources", ReplicatedUsing=OnRep_UltCharge)
+	FGameplayAttributeData UltCharge;
+	OC_ATTRIBUTE_ACCESSORS(UOCAttributeSet_Resources, UltCharge)
+
+	UFUNCTION()
+	void OnRep_UltCharge(const FGameplayAttributeData& OldValue);
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+	
+};
