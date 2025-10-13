f646d1054beb153335f27bab76a9937b16c6ffd4
n-fia <78014168+n-fia@users.noreply.github.com>
2025-10-13 22:19:41 +0900

    [소스] Missile 경로 변경 & 추가

---
+++ OverClock/Source/OverClock/Private/Weapons/OCWhipMissile.cpp
@@ -0,0 +1,91 @@
+#include "Weapons/OCWhipMissile.h"
+
+#include "Components/SphereComponent.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"//사운드 변경 후 삭제
+
+AOCWhipMissile::AOCWhipMissile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+    InitialSpeed = 2000.0f;
+    MaxSpeed = 2500.0f;
+    LifeSpan = 5.0f;
+
+    // 충돌 설정
+    CollisionComponent->SetSphereRadius(20.0f);
+    CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    CollisionComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+    CollisionComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);
+
+	ProjectileMovement->bRotationFollowsVelocity = true; // 날아가는 방향으로 회전
+	ProjectileMovement->ProjectileGravityScale=0.0f;
+}
+
+void AOCWhipMissile::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GetOwner())
+	{
+		CollisionComponent->IgnoreActorWhenMoving(GetOwner(), true);
+	}
+
+	TArray<AActor*> AllWorldActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APawn::StaticClass(), AllWorldActors);
+	FGameplayTag OwnerTag = GetTeamTag(GetOwner());
+	for (AActor* Actor : AllWorldActors)
+	{
+		if (Actor && Actor!=GetOwner())
+		{
+			if (GetTeamTag(Actor) == OwnerTag)
+			{
+				CollisionComponent->IgnoreActorWhenMoving(Actor, true);
+			}
+		}
+	}
+}
+
+void AOCWhipMissile::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+    CollisionComponent->OnComponentHit.AddDynamic(this, &AOCWhipMissile::OnHit);
+}
+
+void AOCWhipMissile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+    FVector NormalImpulse, const FHitResult& Hit)
+{
+    Super::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+
+	if (OtherActor==GetOwner())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[AOCWhipMissile] OtherActor Is Owner"))
+		return;
+	}
+
+    if (!HasAuthority() || !GameEffectClass || !OtherActor) return;
+
+    if (OtherActor && OtherActor->IsA(APawn::StaticClass()))
+    {
+        if (GetTeamTag(OtherActor)==GetTeamTag(GetOwner())) return;
+		
+        if (HitSound)
+        {
+            UGameplayStatics::PlaySoundAtLocation(this, HitSound, Hit.Location);
+        }
+
+        // Damage GE 적용
+    	UAbilitySystemComponent* TargetASC = GetASC(OtherActor);
+    	if (!TargetASC) return;
+        FGameplayEffectContextHandle EffectContext = TargetASC->MakeEffectContext();
+    	TargetASC->ApplyGameplayEffectToSelf(GameEffectClass.GetDefaultObject(), 1, EffectContext);
+        
+        UnInit();
+    }
+}
+
+void AOCWhipMissile::SetVelocity(const FVector& NewVelocity)
+{
+	ProjectileMovement->Velocity = NewVelocity*InitialSpeed;
+}
---
+++ OverClock/Source/OverClock/Public/Weapons/OCPoisonMissile.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/OCMissileBase.h"
+#include "OCPoisonMissile.generated.h"
+
+class UNiagaraComponent;
+/**
+ * 
+ */
+UCLASS()
+class OVERCLOCK_API AOCPoisonMissile : public AOCMissileBase
+{
+	GENERATED_BODY()
+	
+public:
+	AOCPoisonMissile();
+
+	UPROPERTY(EditAnywhere, Category="Effects")
+	TObjectPtr<UNiagaraComponent> NiagaraComp;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	float Damage = 20.0f;
+
+protected:
+	virtual void PostInitializeComponents() override;
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse, const FHitResult& Hit) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage")
+	TSubclassOf<class AOCPoisonField> PoisonFieldClass;
+
+public:	
+	void SetVelocity(const FVector& NewVelocity);
+};
