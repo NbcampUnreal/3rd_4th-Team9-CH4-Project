c4231353cb0520b0992d5999745079de87fcdfba
n-fia <78014168+n-fia@users.noreply.github.com>
2025-10-13 22:15:54 +0900

    pooling 구현

---
+++ OverClock/Source/OverClock/Public/Interface/PooledInterface.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PooledInterface.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UPooledInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class OVERCLOCK_API IPooledInterface
+{
+	GENERATED_BODY()
+
+public:
+	bool bInitial = false;
+
+	virtual void Alloc() = 0;
+	virtual void Init() = 0;
+	virtual void UnInit() = 0;
+	virtual void Release() = 0;
+};
